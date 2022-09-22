edf3d77aa602ae78a3a0184d65a19eb744f4c591
eNV25 <env252525@gmail.com>
2022-09-23 02:56:05 +0530

    Update onepiece.json

---
+++ tcbscans.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+trap -- 'rm -rf "${temps[@]}"' 0
+manga="$(mktemp)"
+temps+=("$manga")
+curl -sSL 'https://onepiecechapters.com/mangas/5/one-piece' >"$manga"
+sum="$(mktemp)"
+temps+=("$sum")
+for n in $(seq "$@"); do
+	chapter="$(mktemp)"
+	temps+=("$chapter")
+	curl -sSL "$(htmlq "main div div div div a:nth-last-child($n)" -a href -b https://onepiecechapters.com <"$manga")" >"$chapter"
+	title="$(htmlq "main div div div div a:nth-last-child($n) div:nth-child(2)" -t <"$manga")"
+	if [ -z "$title" ]; then
+		title="$n"
+	fi
+	htmlq 'main img' -a src <"$chapter" | jq -R --arg n "$n" --arg title "$title" '{ ($n): { $title, groups: { tcbscans: [inputs] } } }'
+done >"$sum"
+jq -s 'add' "$sum"
